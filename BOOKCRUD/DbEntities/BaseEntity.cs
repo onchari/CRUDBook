4274166deeaea71afa38bd04242d26a6a039b157
onchari wycliffe <onchariwycliffe@gmail.com>
2018-04-18 07:32:35 +0300

    FIRSt

---
+++ BOOKCRUD/DbEntities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BOOKCRUD.DbEntities
+{
+    public class BaseEntity
+    {
+        public Int64 Id { get; set; }
+        public DateTime AddedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string IPAddres { get; set; }
+    }
+}
---
+++ BOOKCRUD/DbEntities/BookMap.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BOOKCRUD.DbEntities
+{
+    public class BookMap
+    {
+        public BookMap(EntityTypeBuilder<Book> entityTypeBuilder)
+        {
+            entityTypeBuilder.HasKey(t => t.Id);
+            entityTypeBuilder.Property(name => name.Name).IsRequired();
+            entityTypeBuilder.Property(isbn => isbn.ISBN).IsRequired();
+            entityTypeBuilder.Property(author => author.Author).IsRequired();
+            entityTypeBuilder.Property(publisher => publisher.Publisher).IsRequired();
+        }
+    }
+}
